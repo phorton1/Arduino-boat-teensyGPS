c078dfca4ca58f6202e295cf2ecd664b9ec4e4ee
phorton1 <available@upon.request>
2026-02-04 11:48:40 +0000

    different DEV_QUERIY sub_versions for ST GPS devices;   implemented response to Restart GPS button implementeed newly found ST_TARGET_NAME/ST_TARGET_ID parsing

--- teensyGPS.ino
+++ teensyGPS.ino
@@ -26,7 +26,6 @@
 // Seatalk
 
 #include <instST.h>
-bool g_seatalk_enabled;
 #define ST_IDLE_BUS_MS				10		// ms bus must be idle to send next datagram
 #define ST_SEND_INTERVAL			10
 #define SERIAL_ST2					Serial2
@@ -147,7 +146,7 @@ void setup()
 	
 	loadFromEEPROM();
 
-	initNeo6M_GPS();
+	initNeo6M_GPS(1,0x99);
 
 	SERIAL_ST2.begin(4800, SERIAL_9N1);
 
@@ -212,15 +211,15 @@ static void handleCommand(String lval, String rval, bool got_equals)
 	else if (lval == "seatalk")
 	{
 		bool enable = rval.toInt();
-		if (enable)
-			enableNeoNMEA200(false);
+		// if (enable)
+		// 	enableNeoNMEA200(false);
 		enableNeoSeatalk(enable);
 	}
 	else if (lval == "nmea2000")
 	{
 		bool enable = rval.toInt();
-		if (enable)
-			enableNeoSeatalk(false);
+		// if (enable)
+		// 	enableNeoSeatalk(false);
 		enableNeoNMEA200(enable);
 	}
 
@@ -278,6 +277,107 @@ static void handleSerial()
 
 
 
+//-------------------------------------------------------------
+// Minimal ST parser for DEV_QUERY and Restart GPS Button
+//------------------------------------------------------------
+// Restart GPS button
+
+#define dbg_parse 0
+
+static void parseDatagram(uint8_t *dg)
+{
+	if (dg[0] == 0xa4 &&	// ST_DEV_QUERY == 0x1a4
+	   (dg[1] == 0x06 || dg[1] == 0x02))
+	{
+		warning(dbg_parse,"DEV_QUERY",0);
+		st_neo_device_query_pending = 1;
+	}
+	else if (
+		dg[0] == 0xa5 && 	// ST_SAT_DETAIL = 0x1a5
+		dg[1] == 0x4d &&
+		dg[15] == 0x08)
+	{
+		warning(dbg_parse,"RESTART GPS",0);
+		replyToRestartGPSButton();
+	}
+}
+
+
+
+
+static void handleStPort()
+{
+
+	static uint32_t last_st_read;
+	static uint32_t last_st_write;
+	static int outp = 0;
+	static int dlen = 0;
+	static uint8_t datagram[MAX_ST_BUF];
+
+	// purge the input buffer
+
+	while (SERIAL_ST2.available())
+	{
+		int c = SERIAL_ST2.read();
+		last_st_read = millis();
+
+		// the 9th bit is set on the first 'byte' of a sequence
+		// the low nibble of the 2nd byte + 3 is the total number
+		// 		of bytes, so all messages are at least 3 bytes
+		//		the high nibble may be data.
+		//	data[n+3];, implying a maximum datagram size of 19
+		//  this routine can never receive more than 19 bytes
+		//	but MAX_ST_BUF is set to 20 for neatness
+
+		#if 0
+			display(0,"ST got 0x%02x '%c'",c,(c>32 && c<128)?c:' ');
+		#endif
+
+		if (c > 0xff)
+		{
+			if (outp)
+			{
+				my_error("Dropped datagram ",0);
+				outp = 0;
+			}
+			datagram[outp++] = c;
+		}
+		else if (outp == 1)
+		{
+			dlen = (c & 0x0f) + 3;
+			datagram[outp++] = c;
+		}
+		else if (outp < dlen)
+		{
+			datagram[outp++] = c;
+			if (outp == dlen)
+			{
+				parseDatagram(datagram);
+				outp = 0;
+				dlen = 0;
+			}
+		}
+		else
+		{
+			my_error("unexpected byte 0x%02x '%c'",c,(c>32 && c<128)?c:' ');
+		}
+
+	}	// receiving datagrams
+
+
+	// send one datagram from the ST port's queue
+
+	uint32_t now_st = millis();
+	if (now_st - last_st_read >= ST_IDLE_BUS_MS &&
+		now_st - last_st_write > ST_SEND_INTERVAL)
+	{
+		sendDatagram(E80_PORT2);
+		last_st_write = millis();
+	}
+}
+
+
+
 
 //--------------------------------------------
 // loop()
@@ -291,35 +391,20 @@ void loop()
 	// SEATALK
 	//-------------------
 
-	static uint32_t last_st_read;
-	static uint32_t last_st_write;
-
-	// purge the input buffer
-
-	while (SERIAL_ST2.available())
-	{
-		/* int c = */ SERIAL_ST2.read();
-		last_st_read = millis();
-	}
-	// send one datagram from the ST port's queue
-
-	uint32_t now_st = millis();
-	if (now_st - last_st_read >= ST_IDLE_BUS_MS &&
-		now_st - last_st_write > ST_SEND_INTERVAL)
-	{
-		sendDatagram(E80_PORT2);
-		last_st_write = millis();
-	}
+	handleStPort();
 
 	//---------------------
 	// NMEA200
 	//----------------------
 
-	nmea2000.ParseMessages(); // Keep parsing messages
-	#if BROADCAST_NMEA2000_INFO
-		nmea2000.broadcastNMEA2000Info();
-	#endif
-
+	if (NeoNMEA2000Enabled())
+	{
+		nmea2000.ParseMessages(); // Keep parsing messages
+		#if BROADCAST_NMEA2000_INFO
+			nmea2000.broadcastNMEA2000Info();
+		#endif
+	}
+	
 	//-------------------
 	// UI
 	//-------------------
